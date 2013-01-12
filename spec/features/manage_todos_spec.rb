a211fdce9c8dfe8cadad65bdaf304d5b3ee939b4
Bryan Tomlin <bdtomlin@gmail.com>
2013-01-12 13:08:25 -0600

    added notes for questions

--- spec/features/manage_todos_spec.rb
+++ spec/features/manage_todos_spec.rb
@@ -9,4 +9,16 @@ feature 'Manage Todos' do
 
     expect(page).to have_css 'li.todo', text: 'Buy some underwear'
   end
+
+  scenario 'view only my todos' do
+    Todo.create(description: 'Buy a hat', owner_email: 'not_me@example.com')
+    sign_in_as 'me@example.com'
+
+    click_link 'Add a new todo'
+    fill_in 'Description', with: 'Buy some underwear'
+    click_button 'Create todo'
+
+    expect(page).to have_css 'li.todo', text: 'Buy some underwear'
+    expect(page).not_to have_css 'li.todo', text: 'Buy some underwear'
+  end
 end
