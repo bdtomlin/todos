7b893352e2058ddb8a55b3c18acc81bbcb31fed0
Bryan Tomlin <bdtomlin@gmail.com>
2013-01-20 16:57:27 -0600

    Refactor manage todos spec

--- spec/features/manage_todos_spec.rb
+++ spec/features/manage_todos_spec.rb
@@ -18,23 +18,33 @@ feature 'Manage Todos' do
   scenario 'mark todos as complete' do
     sign_in
     create_todo_with_description 'Buy some underwear'
-    within 'li.todo' do
-      click_link 'Complete'
-    end
+    complete_todo 'Buy some underwear'
     expect(page).to have_css 'li.todo.completed'
   end
 
-  def create_todo_with_description(description)
-    click_link 'Add a new todo'
-    fill_in 'Description', with: description
-    click_button 'Create todo'
-  end
+  private
+    def complete_todo(description)
+      within "li.todo:contains('#{description}')" do
+        click_link 'Complete'
+      end
+      user_sees_completed_todo_item('Buy some underwear')
+    end
 
-  def user_sees_todo_item(description)
-    expect(page).to have_css 'li.todo', text: description
-  end
+    def create_todo_with_description(description)
+      click_link 'Add a new todo'
+      fill_in 'Description', with: description
+      click_button 'Create todo'
+    end
 
-  def user_does_not_see_todo_item(description)
-    expect(page).to_not have_css 'li.todo', text: description
-  end
+    def user_sees_todo_item(description)
+      expect(page).to have_css 'li.todo', text: description
+    end
+
+    def user_sees_completed_todo_item(description)
+      expect(page).to have_css 'li.todo.completed', text: description
+    end
+
+    def user_does_not_see_todo_item(description)
+      expect(page).to_not have_css 'li.todo', text: description
+    end
 end
